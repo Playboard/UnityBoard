de60e9629a52119fceb1aa9c45bc986bdb3fc298
southerncross <lishunyang@mokahr.com>
2019-01-03 00:46:53 +0800

    add some game objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject card;
+    public GameObject board;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 spawnPosition = new Vector3
+            (
+                Random.Range(-10, 10),
+                5.0f,
+                Random.Range(-7, 7)
+            );
+            Instantiate(card, spawnPosition, Quaternion.identity);
+        }
+    }
+}
